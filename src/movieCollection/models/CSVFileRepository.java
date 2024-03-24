11cfef127817915db19a4efa58f4087918e25673
tatjana <tatjana@mail.mail>
2024-03-24 18:25:46 +0100

    Update in CLIView switch-case, InsertCommand, HelpCommand, InfoCommand. Rename some methods in MovieController, delete objects. Add parsFromCSV to Movie, update constructor #2

--- src/movieCollection/models/CSVFileRepository.java
+++ src/movieCollection/models/CSVFileRepository.java
@@ -13,9 +13,10 @@ public class CSVFileRepository {
 
 
     public CSVFileRepository(String filepath) {
-        this.filepath = filepath; // записывает данные элеменов коллекции в файл в формате CSV
+        this.filepath = filepath;
     }
     public void save(Collection<Movie> data) {
+        // записывает данные элементов коллекции в файл в формате CSV
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(filepath))) {
             for (Movie movie : data) {
                 String line = movie.getCSVLine();
@@ -24,31 +25,32 @@ public class CSVFileRepository {
             }
             bw.flush();
         } catch (IOException e) {
-            System.err.println("IOException: " + e.getMessage());
+            System.err.println("IOException: " + e.getMessage()); // выводим ошибку не пользователю, а для нашего понимания
         }
     }
-//    public Collection<Movie> load() {
-//        Collection<Movie> movies = new ArrayList<>();
-//
-//        try (Scanner scanner = new Scanner(new FileInputStream(filepath))) {
-//            while (scanner.hasNext()) {
-//                String line = scanner.nextLine();
-//                if (line.isEmpty()) {
-//                    continue;
-//                }
-//
-//                Movie movie = Movie.parseFromCSV(line);
-//                movies.add(movie);
-//            }
-//            return movies;
-//        } catch (IOException e) {
-//            System.err.println("IOException: " + e.getMessage());
-//            return null;
-//        }
-//        // читает файл
-//        // формирует объекты
-//        // помещает их в список
-//        // и возвращает в качестве значения
-//    }
+    public Collection<Movie> load() {
+        // читает файл
+        // формирует объекты
+        // помещает их в список
+        // и возвращает в качестве значения
+        Collection<Movie> movies = new ArrayList<>();
+
+        try (Scanner scanner = new Scanner(new FileInputStream(filepath))) {
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+                if (line.isEmpty()) {
+                    continue;
+                }
+
+                Movie movie = Movie.parseFromCSV(line);
+                movies.add(movie);
+            }
+            return movies;
+        } catch (IOException e) {
+            System.err.println("IOException: " + e.getMessage());
+            return null;
+        }
+
+    }
 
 }
